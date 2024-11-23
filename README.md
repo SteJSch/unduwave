84e6e62fc7fcb1fb8adaaac05e63dd54f3821b61
jerostan <stefan.schaefer@helmholtz-berlin.de>
2024-11-23 20:36:27 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# unduwave
+Undupy is a python wrapper providing an API to use Wave and Undumag with python.
+
+### Installation
+```
+pip3 install unduwave
+```
+
+### Get started
+Example for the usage of undupy.
+
+```Python
+from apy import my_lib
+
+lib = my_lib()
+res=lib.mult(6)
+print(res)
+```
---
+++ unduwave/__init__.py
@@ -0,0 +1,12 @@
+"""
+Unduwave init
+"""
+
+# from wave_src import wave_main as wm
+from unduwave.unduwave_incl import *
+from unduwave.constants import *
+# import unduwave.wave_src.wave_main as wm
+# from .structure.structure import *
+# from .trajectory.trajectory import *
+# from .numerics.numerics_main import *
+
