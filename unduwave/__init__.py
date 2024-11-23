9f2b919bbddaafe8610926b9f0572dca45f077ff
jerostan <stefan.schaefer@helmholtz-berlin.de>
2024-11-23 21:15:51 +0100

    hello world

--- unduwave/__init__.py
+++ unduwave/__init__.py
@@ -5,6 +5,7 @@ Unduwave init
 # from wave_src import wave_main as wm
 from unduwave.unduwave_incl import *
 from unduwave.constants import *
+from unduwave.api.api_root import *
 # import unduwave.wave_src.wave_main as wm
 # from .structure.structure import *
 # from .trajectory.trajectory import *
---
+++ unduwave/api/api_root.py
@@ -0,0 +1,7 @@
+"""
+Contains the functionality for loading and processing b-field data
+"""
+from unduwave.unduwave_incl import *
+
+def hello() : 
+	print("hello")
