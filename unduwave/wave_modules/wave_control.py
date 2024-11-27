780c4c7dce00f943292ca6225142108d84fb811d
jerostan <stefan.schaefer@helmholtz-berlin.de>
2024-11-27 13:57:42 +0100

    [update2]

--- unduwave/wave_modules/wave_control.py
+++ unduwave/wave_modules/wave_control.py
@@ -1,5 +1,12 @@
 from unduwave.unduwave_incl import *
 
+try :
+	# works when calling script with python3 script_file
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+except:
+	# works when calling script with exec from python console
+	dir_path = os.getcwd()
+
 class wave_control():
 	"""API for the WAVE program
 
@@ -20,6 +27,10 @@ class wave_control():
 		"""        
 		os.chdir(self.wave_folder + 'stage/' )
 		if os.name == 'nt' :
+			pdb.set_trace()
+			with open(folder+"wave.out", 'r') as o_f:
+				# read an store all lines into list
+				wave_out_file = o_f.readlines()
 			subprocess.call("C:\\cygwin64\\bin\\bash.exe --login -c 'cd unduwave/External-Software/WAVE/stage; ../bin/wave.exe'")        
 		else:
 			os.system("../bin/wave.exe")        
